bf2e4b794a3372fbc62f18b84bc1b11db0c5506d
zackattacknyu <zackattacknyu@gmail.com>
2014-01-24 16:52:08 -0800

    copied the main file

---
+++ opengl4_4/main_fromAlex.cpp
@@ -0,0 +1,400 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "glew\include\GL\glew.h"
+#include "glut\glut.h"
+#include <memory.h>
+#include <math.h>
+#include "mymath.h"
+ 
+#define PI       3.14159265358979323846
+
+void setTransMatrix(float *mat, float x, float y, float z);
+void multiplyMatrix(float *a, float *b);
+void xProduct( float *a, float *b, float *res);
+void normalize(float *a);
+void setIdentMatrix( float *mat, int size);
+void placeCam(float posX, float posY, float posZ, float lookX, float lookY, float lookZ);
+void setTransMatrix(float *mat, float x, float y, float z);
+float * rotationMatrix(float x, float y, float z, float angle);
+
+struct vec3
+{
+	float x;
+	float y;
+	float z;
+};
+ 
+// vertices for triangle
+float vertices1[] = {   -1.0f, -1.0f, -5.0f,
+            1.0f, -1.0f, -5.0f,
+            0.0f, 1.0f, -5.0f};
+ 
+float colors1[] = { 1.0f, 0.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f,0.0f, 1.0f, 1.0f};
+ 
+ 
+// shader names
+char *vertexFileName = "vertex.txt";
+char *fragmentFileName = "frag.txt";
+ 
+// program and shader Id
+GLuint p,v,f;
+ 
+// vert attrib locations
+GLuint vertexLoc, colorLoc;
+ 
+// uniform var locations
+GLuint projMatrixLoc, viewMatrixLoc;
+ 
+// vert array obj Id
+GLuint vert[3];
+ 
+// storage for matrices
+float projMatrix[16];
+float viewMatrix[16];
+
+int frame=0,time,timebase=0;
+char s[50];
+ 
+// vector opt
+// res = a cross b;
+void xProduct( float *a, float *b, float *res) 
+{
+ 
+    res[0] = a[1] * b[2]  -  b[1] * a[2];
+    res[1] = a[2] * b[0]  -  b[2] * a[0];
+    res[2] = a[0] * b[1]  -  b[0] * a[1];
+}
+ 
+// normalize a vec3
+void normalize(float *a) 
+{
+ 
+    float mag = sqrt(a[0] * a[0]  +  a[1] * a[1]  +  a[2] * a[2]);
+    a[0] /= mag;
+    a[1] /= mag;
+    a[2] /= mag;
+}
+
+// Matrix Opt. - In Opengl 3 we need to handle our own matrix.
+ 
+// In this form : a = a * b; 
+void multiplyMatrix(float *a, float *b) 
+{
+    float res[16];
+ 
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            res[j*4 + i] = 0.0f;
+            for (int k = 0; k < 4; ++k) {
+                res[j*4 + i] += a[k*4 + i] * b[j*4 + k];
+            }
+        }
+    }
+    memcpy(a, res, 16 * sizeof(float));
+}
+
+// sets the square matrix mat to the ID matrix,
+void setIdentMatrix( float *mat, int size) 
+{
+ 
+    // 0s
+    for (int i = 0; i < size * size; ++i)
+            mat[i] = 0.0f;
+ 
+    // diagonal 1s
+    for (int i = 0; i < size; ++i)
+        mat[i + i * size] = 1.0f;
+}
+ 
+// View Matrix
+// just like glulookat
+void placeCam(float posX, float posY, float posZ, float lookX, float lookY, float lookZ) 
+{
+ 
+    float dir[3], right[3], up[3];
+ 
+    up[0] = 0.0f;   up[1] = 1.0f;   up[2] = 0.0f;
+ 
+    dir[0] =  (lookX - posX);
+    dir[1] =  (lookY - posY);
+    dir[2] =  (lookZ - posZ);
+    normalize(dir);
+ 
+	// this order matters
+    xProduct(dir,up,right);
+    normalize(right);
+ 
+    xProduct(right,dir,up);
+    normalize(up);
+ 
+    float aux[16];
+ 
+    viewMatrix[0]  = right[0];
+	viewMatrix[1]  = up[0];
+	viewMatrix[2]  = -dir[0];
+	viewMatrix[3]  = 0.0f;
+    viewMatrix[4]  = right[1];
+	viewMatrix[5]  = up[1];
+	viewMatrix[6]  = -dir[1];
+	viewMatrix[7]  = 0.0f;
+    viewMatrix[8]  = right[2];
+	viewMatrix[9]  = up[2];
+	viewMatrix[10] = -dir[2];
+	viewMatrix[11] = 0.0f;
+    viewMatrix[12] = 0.0f;
+    viewMatrix[13] = 0.0f;
+    viewMatrix[14] =  0.0f;
+    viewMatrix[15] = 1.0f;
+    setTransMatrix(aux, -posX, -posY, -posZ);
+    multiplyMatrix(viewMatrix, aux);
+	
+	// you should do this instead. If you want to apply rotation to your viewMatrix.
+	//multiplyMatrix(viewMatrix, rotationMatrix(0.0,0.0,1.0, 0.785)); 
+}
+
+// Generates a rotation matrix.  Angle is in radian.
+float * rotationMatrix(float x, float y, float z, float angle)
+{
+	float tempVec[3];
+	tempVec[0] = x;
+	tempVec[1] = y;
+	tempVec[2] = z;
+    normalize(tempVec);
+
+	vec3 axis;
+	axis.x = tempVec[0];
+	axis.y = tempVec[1];
+	axis.z = tempVec[2];
+
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+
+	float mat[16] = {oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+				0.0,                                0.0,                                0.0,                                1.0};
+
+    return mat;
+
+}
+ 
+// Projection Matrix
+void buildProjMatrix(float fov, float ratio, float nearP, float farP) {
+ 
+    float f = 1.0f / tan (fov * (PI / 360.0));
+    setIdentMatrix(projMatrix,4);
+    projMatrix[0] = f / ratio;
+    projMatrix[1 * 4 + 1] = f;
+    projMatrix[2 * 4 + 2] = (farP + nearP) / (nearP - farP);
+    projMatrix[3 * 4 + 2] = (2.0f * farP * nearP) / (nearP - farP);
+    projMatrix[2 * 4 + 3] = -1.0f;
+    projMatrix[3 * 4 + 3] = 0.0f;
+}
+
+// Transformation matrix mat with a translation
+void setTransMatrix(float *mat, float x, float y, float z) {
+ 
+    setIdentMatrix(mat,4);
+    mat[12] = x;
+    mat[13] = y;
+    mat[14] = z;
+}
+
+void changeSize(int w, int h) {
+ 
+    float ratio;
+
+    // place viewport to be the entire window
+    glViewport(0, 0, w, h);
+    ratio = (1.0f * w) / h;
+    buildProjMatrix(53.13f, ratio, 1.0f, 30.0f);
+}
+ 
+void setupBuffers() {
+ 
+    GLuint buffers[2];
+ 
+    glGenVertexArrays(1, vert);
+
+    // first triangle
+    glBindVertexArray(vert[0]);
+    // generate 2 buffers for vert and color
+    glGenBuffers(2, buffers);
+    // bind buffer for vertices and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vertexLoc);
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, 0, 0, 0);
+ 
+    // bind buffer for colors and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors1), colors1, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(colorLoc);
+    glVertexAttribPointer(colorLoc, 4, GL_FLOAT, 0, 0, 0);
+}
+ 
+void setUniforms() {
+ 
+    // must be called after glUseProgram
+	// set the variables for the shader
+    glUniformMatrix4fv(projMatrixLoc,  1, false, projMatrix);
+    glUniformMatrix4fv(viewMatrixLoc,  1, false, viewMatrix);
+}
+ 
+void renderScene(void) {
+
+	frame++;
+	time=glutGet(GLUT_ELAPSED_TIME);
+	if (time - timebase > 1000) {
+		sprintf(s,"FPS:%4.2f",
+			frame*1000.0/(time-timebase));
+		timebase = time;
+		frame = 0;
+	}
+    glutSetWindowTitle(s);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ 
+    //placeCam(10,2,10,0,2,-5);
+	placeCam(0,0,0,0,0,-5);
+    glUseProgram(p);
+    setUniforms();
+ 
+    glBindVertexArray(vert[0]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+  
+   glutSwapBuffers();
+}
+ 
+
+void printShaderInfoLog(GLuint obj)
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+ 
+    glGetShaderiv(obj, GL_INFO_LOG_LENGTH,&infologLength);
+ 
+    if (infologLength > 0)
+    {
+        infoLog = (char *)malloc(infologLength);
+        glGetShaderInfoLog(obj, infologLength, &charsWritten, infoLog);
+        printf("%s\n",infoLog);
+        free(infoLog);
+    }
+}
+ 
+void printProgramInfoLog(GLuint obj)
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+ 
+    glGetProgramiv(obj, GL_INFO_LOG_LENGTH,&infologLength);
+ 
+    if (infologLength > 0)
+    {
+        infoLog = (char *)malloc(infologLength);
+        glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
+        printf("%s\n",infoLog);
+        free(infoLog);
+    }
+}
+ 
+GLuint initShaders() {
+ 
+    char *vertShader = NULL,*fragShader = NULL;
+ 
+    GLuint p,v,f;
+ 
+    v = glCreateShader(GL_VERTEX_SHADER);
+    f = glCreateShader(GL_FRAGMENT_SHADER);
+    vertShader = getTxtFile(vertexFileName);
+    fragShader = getTxtFile(fragmentFileName);
+    const char * vv = vertShader;
+    const char * ff = fragShader;
+    glShaderSource(v, 1, &vv,NULL);
+    glShaderSource(f, 1, &ff,NULL);
+    free(vertShader);
+	free(fragShader);
+    glCompileShader(v);
+    glCompileShader(f);
+    printShaderInfoLog(v);
+    printShaderInfoLog(f);
+    p = glCreateProgram();
+    glAttachShader(p,v);
+    glAttachShader(p,f);
+    glBindFragDataLocation(p, 0, "outputF");
+    glLinkProgram(p);
+    printProgramInfoLog(p);
+    vertexLoc = glGetAttribLocation(p,"position");
+    colorLoc = glGetAttribLocation(p, "color"); 
+    projMatrixLoc = glGetUniformLocation(p, "projMatrix");
+    viewMatrixLoc = glGetUniformLocation(p, "viewMatrix");
+ 
+    return(p);
+}
+
+float deltaAngle = 0.0f;
+int xOrigin = -1;
+
+float angle = 0.0f;
+
+//This event will trigger when you have a mouse button pressed down.
+void mouseMove(int x, int y) 
+{
+	// x and y is the mouse position.
+}
+
+//This event occur when you press a mouse button.
+void mouseButton(int button, int state, int x, int y) 
+{
+	// only start motion if the left button is pressed
+	if (button == GLUT_LEFT_BUTTON) 
+	{
+		// when the button is released
+		if (state == GLUT_UP) 
+		{
+		}
+		else  // state = GLUT_DOWN	
+		{
+		}
+	}
+}
+ 
+int main(int argc, char **argv) 
+{
+	// sets up glut
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(640,480);
+    glutCreateWindow("ICS Graphics");
+    glutSetWindowTitle(s);
+	// call back functions
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene);
+    glutReshapeFunc(changeSize);
+
+	glutMouseFunc(mouseButton);
+	glutMotionFunc(mouseMove);
+ 
+	// check if a particular extension is available on your platform
+    glewInit();
+    if (glewIsSupported("GL_VERSION_3_3"))
+        printf("OpenGL 3.3 is supported\n");
+    else 
+	{
+        printf("OpenGL 3.3 not supported\n");
+        exit(1);
+    }
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(1.0,1.0,1.0,1.0);
+    p = initShaders(); 
+    setupBuffers(); 
+    glutMainLoop();
+ 
+    return(0); 
+}
