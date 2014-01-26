84f09a7e69299904acebb12f303e11e10471c532
zackattacknyu <zackattacknyu@gmail.com>
2014-01-26 15:28:37 -0800

    began generating vertices for other triangles

--- opengl4_4/main.cpp
+++ opengl4_4/main.cpp
@@ -30,7 +30,11 @@ struct vec3
 // vertices for triangle
 float vertices1[] = {   -1.0f, -1.0f, -5.0f,
             1.0f, -1.0f, -5.0f,
-            0.0f, 1.0f, -5.0f};
+            0.0f, 1.0f, -4.0f};
+
+float vertices2[] = {   -1.0f, -1.0f, -3.0f,
+            1.0f, -1.0f, -3.0f,
+            0.0f, 1.0f, -4.0f};
  
 float colors1[] = { 1.0f, 1.0f, 0.0f, 1.0f,
             1.0f, 1.0f, 0.0f, 1.0f,
