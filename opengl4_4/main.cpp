5dbd349dd595c80e69aa433e7643771ae7aa4b53
zackattacknyu <zackattacknyu@gmail.com>
2014-01-28 22:02:10 -0800

    added in the load bmp custom code

--- opengl4_4/main.cpp
+++ opengl4_4/main.cpp
@@ -19,6 +19,7 @@ void setIdentMatrix( float *mat, int size);
 void placeCam(float posX, float posY, float posZ, float lookX, float lookY, float lookZ);
 void setTransMatrix(float *mat, float x, float y, float z);
 float * rotationMatrix(float x, float y, float z, float angle);
+GLuint loadBMP_custom(char filename[]);
 
 struct vec3
 {
@@ -621,11 +622,80 @@ int main(int argc, char **argv)
         printf("OpenGL 3.3 not supported\n");
         exit(1);
     }
+
+	char* filename = "chessBoard_wood_bmp.bmp";
+	GLuint texturePointer = loadBMP_custom(filename);
+	printf("%d",texturePointer);
+
     glEnable(GL_DEPTH_TEST);
     glClearColor(1.0,1.0,1.0,1.0);
     p = initShaders(); 
     setupBuffers(); 
     glutMainLoop();
+
+	
  
     return(0); 
+}
+
+// From http://www.opengl-tutorial.org/beginners-tutorials/tutorial-5-a-textured-cube/ 
+GLuint loadBMP_custom(char filename[]){
+
+    // Data read from the header of the BMP file
+    unsigned char header[54]; // Each BMP file begins by a 54-bytes header
+    unsigned int dataPos;     // Position in the file where the actual data begins
+    unsigned int width, height;
+    unsigned int imageSize;   // = width*height*3
+    // Actual RGB data
+    unsigned char * data;
+
+
+    FILE * file = fopen(filename,"rb");
+
+    if ( fread(header, 1, 54, file)!=54 ){ // If not 54 bytes read : problem
+        printf("Not a correct BMP file\n");
+        return false;
+    }
+
+    if ( header[0]!='B' || header[1]!='M' ){
+        printf("Not a correct BMP file\n");
+        return 0;
+    }
+
+    printf("\nInside bmp_reader ");
+
+    // Read ints from the byte array
+    dataPos    = *(int*)&(header[0x0A]);
+    imageSize  = *(int*)&(header[0x22]);
+    width      = *(int*)&(header[0x12]);
+    height     = *(int*)&(header[0x16]);
+
+    // Some BMP files are misformatted, guess missing information
+    if (imageSize==0)    imageSize=width*height*3; // 3 : one byte for each Red, Green and Blue component
+    if (dataPos==0)      dataPos=54; // The BMP header is done that way
+
+    // Create a buffer
+    data = new unsigned char [imageSize];
+
+    // Read the actual data from the file into the buffer
+    fread(data,1,imageSize,file);
+
+    //Everything is in memory now, the file can be closed
+    fclose(file);
+
+    // Create one OpenGL texture
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // "Bind" the newly created texture : all future texture functions will modify this texture
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Give the image to OpenGL
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    return textureID;
+
 }
