0dea5c7abf9980afe4c2067b8becea846cbf341f
zackattacknyu <zackattacknyu@gmail.com>
2014-01-30 18:39:34 -0800

    adjusted camera movements

--- opengl4_4/main.cpp
+++ opengl4_4/main.cpp
@@ -884,21 +884,27 @@ void readKeyboard( unsigned char key, int x, int y ){
 		  //camera movements
 	  case 'i':
 		  cameraZ = cameraZ - 0.1;
+		  lookAtZ = lookAtZ - 0.1;
 		  break;
 	  case 'k':
 		  cameraZ = cameraZ + 0.1;
+		  lookAtZ = lookAtZ + 0.1;
 		  break;
 	  case 'j':
 		  cameraX = cameraX + 0.1;
+		  lookAtX = lookAtX + 0.1;
 		  break;
 	  case 'l':
 		  cameraX = cameraX - 0.1;
+		  lookAtX = lookAtX - 0.1;
 		  break;
 	  case 'y':
 		  cameraY = cameraY + 0.1;
+		  lookAtY = lookAtY + 0.1;
 		  break;
 	  case 'h':
 		  cameraY = cameraY - 0.1;
+		  lookAtY = lookAtY - 0.1;
 		  break;
 
 		  //look at movements
